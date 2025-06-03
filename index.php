221a920924826d9a5c149e6b0cd4c37052038b42
DouglasVbr <douglascanal1998@gmail.com>
2025-06-02 22:10:05 -0300

    exercicios

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="title">
+
+        <h1>Exercícios de PHP</h1>
+        <h2>Curso de PHP - Exercícios</h2>
+    </div>
+<div class="container">
+    <div class="link"><a href="./exer1/exer1.php"></a></div>
+    <div class="link"><a href="./exer2/exer2"></a></div>
+    <div class="link"><a href="./exer2/exer3"></a></div>
+    <div class="link"><a href="./exer4/exer4"></a></div>
+    <div class="link"><a href="./exer5/exer5"></a></div>
+    <div class="link"><a href="./exer6/exer6"></a></div>
+    <div class="link"><a href="./exer7/exer7"></a></div>
+    <div class="link"><a href="./exer8/exer8"></a></div>
+    <div class="link"><a href="./exer9/exer9"></a></div>
+    <div class="link"><a href="./exer10/exer10"></a></div>
+    <div class="link"><a href="./exer11/exer11"></a></div>
+</div>
+</body>
+</html>
