ecf939bdf2a083650b12542685df09e2ce8b1ea4
DouglasVbr <douglascanal1998@gmail.com>
2025-06-04 20:14:32 -0300

    stylo css nas paginas

--- exer1/exer1.php
+++ exer1/exer1.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>Exercício 1 - Par ou Ímpar</title>
 </head>
 <body>
